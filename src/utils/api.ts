26648fe9285fb714778976d6a6eeb7e64da4fd90
anoopsingh1996 <anoopraghuvansi@gmail.com>
2023-01-15 17:33:16 +0530

    weather chrome extension

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,34 +1,45 @@
-const OPEN_WETHER_API_KEY = '8546afb4c7d7f34c35b4e92c346a3c51';
+const OPEN_WEATHER_API_KEY = '8546afb4c7d7f34c35b4e92c346a3c51'
 
 export interface OpenWeatherData {
-    name: string
-    main: {
-        feels_like: number
-        humidity:number
-        pressure:number
-        temp:number
-        temp_max:number
-        temp_min:number
-    }
-    weather: {
-        description:string
-        icon:string
-        id:number
-        main:string
-    }[]
-    wind: {
-        deg:number
-        speed:number
-    }
+  name: string
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+  }
 }
 
-export async function fetchOpenWeatherData(city: string): Promise<OpenWeatherData> {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WETHER_API_KEY}`)
+export type OpenWeatherTempScale = 'metric' | 'imperial'
 
-    if(!res.ok) {
-        throw new Error('City not found');
-    }
+export async function fetchOpenWeatherData(
+  city: string,
+  tempScale: OpenWeatherTempScale
+): Promise<OpenWeatherData> {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${tempScale}&appid=${OPEN_WEATHER_API_KEY}`
+  )
 
-    const data: OpenWeatherData = await res.json();
-    return data;
+  if (!res.ok) {
+    throw new Error('City not found')
+  }
+
+  const data: OpenWeatherData = await res.json()
+  return data
+}
+
+export function getWeatherIconSrc(iconCode: string) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`
 }
